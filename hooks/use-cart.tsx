5675575fdb81bd34f53c7ee10e2cfd3ee07d1897
dp codes <124174236+Irasubiza1@users.noreply.github.com>
2023-09-21 15:49:11 +0200

    divin9

--- hooks/use-cart.tsx
+++ hooks/use-cart.tsx
@@ -12,29 +12,42 @@ interface CartStore {
   removeItem: (id: string) => void;
   toogleCartItemQuantity: (id: string, value: string) => void;
   removeAll: () => void;
+  totalQuantity: number;
+  totalPrice: number;
 }
+let foundProduct;
+let index;
 const useCart = create(
   persist<CartStore>((set, get) => ({
     items: [],
+    totalPrice: 0,
+    totalQuantity: 0,
     addItem: (data: Product, quantity: number) => {
       const currentItems = get().items;
       const existingItem = currentItems.find((item) => item.id === data.id);
+      set({ totalQuantity: get().totalQuantity + quantity })
+      set({ totalPrice: get().totalPrice + Number(data.price)})
 
       if (existingItem) {
         return toast('Item already in the cart');
       }
-
+      
       set({ items: [...get().items, { ...data, quantity }] });
       toast.success('Item added to cart.');
     },
     removeItem: (id: string) => {
+      const currentItems = get().items;
+      foundProduct = currentItems.find((item) => item.id === id);
+      if (foundProduct) {
+        set({ totalQuantity: get().totalQuantity - foundProduct?.quantity })
+      }
       set({ items: [...get().items.filter((item) => item.id !== id)] });
       toast.success('Item removed from cart.');
     },
     toogleCartItemQuantity(id, value) {
       const currentItems = get().items;
-      let foundProduct = currentItems.find((item) => item.id === id);
-      let index = currentItems.findIndex((product) => product.id === id);
+      foundProduct = currentItems.find((item) => item.id === id);
+      index = currentItems.findIndex((product) => product.id === id);
       if (!foundProduct) {
         return
       }
@@ -44,10 +57,10 @@ const useCart = create(
         set({ items: [...get().items] })
       } else if (value === "dec") {
         if (foundProduct.quantity > 1) {
-        foundProduct.quantity--
-        currentItems[index] = foundProduct
-        set({ items: [...get().items] })
-      }
+          foundProduct.quantity--
+          currentItems[index] = foundProduct
+          set({ items: [...get().items] })
+        }
       }
     },
     removeAll: () => set({ items: [] }),
